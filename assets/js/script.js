9fd0f3df7fc946181affcab70042c1e55cbe0643
Vanessa Patterson <vpatt1031@gmail.com>
2021-03-24 20:44:15 -0400

    updated work page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,64 @@
-$( document ).ready(function() {
-                
-                $("#about_scroll").fadeOut();   
-                $("#work_scroll").fadeOut();
-                $("#contact_scroll").fadeOut();
+$(document).ready(function () {
+  $("#about_scroll").fadeOut();
+  $("#work_scroll").fadeOut();
+  $("#contact_scroll").fadeOut();
 
-                $("#about").click(function(){
-                    $("#index").fadeOut();
-                    $("#about_scroll").fadeIn();
-                    $('#about_left').addClass('animated slideInLeft');
-                    $('#about_right').addClass('animated slideInRight');
-                    });
-                $("#work").click(function(){
-                    $("#index").fadeOut();
-                    $("#work_scroll").fadeIn();
-                    $('#work_left').addClass('animated slideInLeft');
-                    $('#work_right').addClass('animated slideInRight');
-                    });
-                $("#contact").click(function(){
-                    $("#index").fadeOut();
-                    $("#contact_scroll").fadeIn();
-                    $('#contact_left').addClass('animated slideInLeft');
-                    $('#contact_right').addClass('animated slideInRight');
-                    });
-                
-                $(".back").click(function(){
-                    $(".pages").fadeOut();
-                    $("#index").fadeIn();
-                    $('#index_left').addClass('animated slideInLeft');
-                    $('#index_right').addClass('animated slideInRight');
-                    });
-           
-		});
+  $("#about").click(function () {
+    $("#index").fadeOut();
+    $("#about_scroll").fadeIn();
+    $("#about_left").addClass("animated slideInLeft");
+    $("#about_right").addClass("animated slideInRight");
+  });
+  $("#work").click(function () {
+    $("#index").fadeOut();
+    $("#work_scroll").fadeIn();
+    $("#work_left").addClass("animated slideInLeft");
+    $("#work_right").addClass("animated slideInRight");
+  });
+  $("#contact").click(function () {
+    $("#index").fadeOut();
+    $("#contact_scroll").fadeIn();
+    $("#contact_left").addClass("animated slideInLeft");
+    $("#contact_right").addClass("animated slideInRight");
+  });
+
+  $(".back").click(function () {
+    $(".pages").fadeOut();
+    $("#index").fadeIn();
+    $("#index_left").addClass("animated slideInLeft");
+    $("#index_right").addClass("animated slideInRight");
+  });
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
