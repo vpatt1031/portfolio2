8f2ebeebb0da4bb80cc9ed412d18c473e3b22a3c
Vanessa Patterson <vpatt1031@gmail.com>
2021-03-24 21:48:34 -0400

    added changes to create a functioning contact form, now I receive submissions to my email

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,39 +1,59 @@
-function validate(){
-    var name = document.getElementById("name").value;
-    var subject = document.getElementById("subject").value;
-    var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    var error_message = document.getElementById("error_message");
+// function validate(){
+//     var name = document.getElementById("name").value;
+//     var subject = document.getElementById("subject").value;
+//     var phone = document.getElementById("phone").value;
+//     var email = document.getElementById("email").value;
+//     var message = document.getElementById("message").value;
+//     var error_message = document.getElementById("error_message");
     
-    error_message.style.padding = "10px";
+//     error_message.style.padding = "10px";
     
-    var text;
-    if(name.length < 5){
-      text = "Please Enter valid Name";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(subject.length < 10){
-      text = "Please Enter Correct Subject";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(isNaN(phone) || phone.length != 10){
-      text = "Please Enter valid Phone Number";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(email.indexOf("@") == -1 || email.length < 6){
-      text = "Please Enter valid Email";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(message.length <= 140){
-      text = "Please Enter More Than 140 Characters";
-      error_message.innerHTML = text;
-      return false;
-    }
-    alert("Form Submitted Successfully!");
-    return true;
-  }
+//     var text;
+//     if(name.length < 5){
+//       text = "Please Enter valid Name";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(subject.length < 10){
+//       text = "Please Enter Correct Subject";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(isNaN(phone) || phone.length != 10){
+//       text = "Please Enter valid Phone Number";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(email.indexOf("@") == -1 || email.length < 6){
+//       text = "Please Enter valid Email";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(message.length <= 140){
+//       text = "Please Enter More Than 140 Characters";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     alert("Form Submitted Successfully!");
+//     return true;
+//   }
+// var form = document.getElementById("form_edit");
+            
+//             async function handleSubmit(event) {
+//               event.preventDefault();
+//               var status = document.getElementById("my-form-status");
+//               var data = new FormData(event.target);
+//               fetch(event.target.action, {
+//                 method: form.method,
+//                 body: data,
+//                 headers: {
+//                     'Accept': 'application/json'
+//                 }
+//               }).then(response => {
+//                 status.innerHTML = "Thanks for your submission!";
+//                 form.reset()
+//               }).catch(error => {
+//                 status.innerHTML = "Oops! There was a problem submitting your form"
+//               });
+//             }
+//             form.addEventListener("submit", handleSubmit)
